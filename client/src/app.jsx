77da436b13022fb48ec7eb1038df2c8f46d84cfb
Sudipta <sudiptopatra80@gmail.com>
2026-03-02 07:16:52 +0530

    update server url

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-server-ao07.onrender.com");
-
+cgit add .
 const COLORS = [
   "#FF6B6B", "#4ECDC4", "#45B7D1", "#96CEB4",
   "#FECA57", "#FF9FF3", "#54A0FF", "#5F27CD",
